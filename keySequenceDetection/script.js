f67c8a63ddec2f3aee9e680611e896c984e865ab
icamp <campan.ionel@gmail.com>
2020-04-11 12:19:40 +0300

    Fixed remaining missing links to source code & added some comments where needed

--- keySequenceDetection/script.js
+++ keySequenceDetection/script.js
@@ -1,20 +1,20 @@
 
+const pressed = [];        // an empty array to which all the pressed keys will be pushed
+const secretCode = 'yes'   // the word that triggers events
 
-  const pressed = [];           // an empty array to which all the pressed keys will be pushed
-  const secretCode = 'yes'   // the word we are looking for
-  window.addEventListener('keyup', (e) => {
-    // console.log(e);   // shows which key was pressed
-    pressed.push(e.key);  // pushes the pressed key to the array
+window.addEventListener('keyup', (e) => {
+  // console.log(e);   // shows which key was pressed
+  pressed.push(e.key);  // pushes the pressed key to the array
 
-    /*
-      the array is trimmed with 'pressed.splice'
-      in '-secretCode.length -1', '-' (negative) will count from the back (from right to left) the length of secretCode +1 (which is where trimming will start),
-      then 'pressed.length - secretCode.length' will give the exact number that is needed
-    */ 
-    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length); 
-    if(pressed.join('').includes(secretCode)) { //  pressed.join('') turns the array into a string
-      console.log("DING! DING!")
-      cornify_add();
-    }
-    // console.log(pressed);
-  })
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length); 
+  if(pressed.join('').includes(secretCode)) { //  pressed.join('') turns the array into a string
+      /*
+        the array is trimmed with 'pressed.splice'
+        in '-secretCode.length -1', '-' (negative) will count from the back (from right to left) the length of secretCode +1 (which is where trimming will start),
+        then 'pressed.length - secretCode.length' will give the exact number that is needed
+      */ 
+  // console.log("DING! DING!")
+    cornify_add();
+  }
+  // console.log(pressed);
+})
--- slideInOnScroll/script.js
+++ slideInOnScroll/script.js
@@ -1,3 +1,5 @@
+// a function captured from the wild internet.
+// to reduce how often functions run on scroll, for performance reasons
 function debounce(func, wait = 20, immediate = true) {
     var timeout;
     return function() {
@@ -16,7 +18,7 @@ function debounce(func, wait = 20, immediate = true) {
   // select all the images that will be listened
   const sliderImages = document.querySelectorAll('.slide-in');
 
-  // write a function that will run everytime the user scrolls
+  // a function that will run everytime the user scrolls
   function checkslide(e) {
     // console.count(e);
     // loop over every image and calculate where it needs to be shown
@@ -24,12 +26,13 @@ function debounce(func, wait = 20, immediate = true) {
     sliderImages.forEach(sliderImage => {
       const slideInAt = (window.scrollY + window.innerHeight) // the pixel level at the bottom of the viewport, 
                                                               // where the sliding should start 
-                          - sliderImage.height / 2; // halfway through the image
-      console.log(slideInAt);
+                          - sliderImage.height / 2;           // halfway through the image
 
-      const imageBottom = sliderImage.offsetTop + sliderImage.height; //offsetTop is the distance from top of page to top of img
-      const isHalfShown = slideInAt > sliderImage.offsetTop; // make sure that the slideInAt value is greater than the top of where the actual img is
-      const isNotScrolledPast = window.scrollY < imageBottom; // make sure we're not scrolled all the way past the image
+      // console.log(slideInAt); 
+
+      const imageBottom = sliderImage.offsetTop + sliderImage.height;  // offsetTop is the distance from top of page to top of img
+      const isHalfShown = slideInAt > sliderImage.offsetTop;           // make sure that the slideInAt value is greater than the top of where the actual img is
+     const isNotScrolledPast = window.scrollY < imageBottom;           // make sure we're not scrolled all the way past the image
 
       if (isHalfShown && isNotScrolledPast) {
         sliderImage.classList.add('active');
